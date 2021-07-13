267cbe923a6209ae73fcead8e5ed330a32d2cb75
Quincy Chang <quincy0710@hotmail.com>
2021-07-13 12:59:11 +0800

    chore: modify template wording

--- script-generator/src/main/java/com/coolbitx/wallet/signing/main/main.java
+++ script-generator/src/main/java/com/coolbitx/wallet/signing/main/main.java
@@ -8,13 +8,13 @@ public class main {
 
     public static void main(String[] args) throws Exception {
 
-        // Step 1. Define Arguments
+        // Step 1. Define Arguments.
         ScriptArgumentComposer sac = new ScriptArgumentComposer();
         ScriptBuffer argTo = sac.getArgument(20);
         ScriptBuffer argValue = sac.getArgumentRightJustified(10);
         ScriptBuffer argDecimal = sac.getArgument(1);
 
-        // Step 2. Set up Script Header
+        // Step 2. Set up Script Header.
         // length | version | hash | sign
         String header =  "03040601";
         // length: 03
@@ -22,10 +22,10 @@ public class main {
         // hash: choose one in ScriptAssembler
         // sign: 01 (ECDSA), 02 (EDDSA)
 
-        // Step 3. Define BIP-44/SLIP0010 CoinType for validation in the path
+        // Step 3. Set BIP-44/SLIP0010 CoinType for validation to the path.
         String coinType = ScriptAssembler.setCoinType(0x3C);
 
-        // Step 4. Compose the raw transaction for signing
+        // Step 4. Compose the raw transaction from arguments for signing.
         String payload = ScriptAssembler.copyString("02")
                 + ScriptAssembler.arrayPointer()
                 + ScriptAssembler.copyString("94")
@@ -34,7 +34,7 @@ public class main {
                 + ScriptAssembler.copyString("C0")
                 + ScriptAssembler.arrayEnd(1);
 
-        // Step 5. Define which parts of the arguments shall be showed on the screen
+        // Step 5. Define which parts of the arguments shall be showed on the screen to be validated.
         String display = ScriptAssembler.showMessage("TEMPLATE")
                 + ScriptAssembler.setBufferInt(argDecimal, 0, 20)
                 + ScriptAssembler.showAmount(argValue, 1000)
@@ -44,5 +44,14 @@ public class main {
 	      System.out.println("\n============================== Script Start ==============================\n");
 	      System.out.println(script);
 	      System.out.println("\n============================== Script End ==============================\n");
+	      System.out.println("Please copy the above script to test in the script-tester.\n");
+	      System.out.println("The argument input should be a hex string which composed of");
+        System.out.println("the arguments in the order you defined.");
+        System.out.println("\nFor example, below arguments");
+        System.out.println("  to: 86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0");
+        System.out.println("  value: 000000004563918244f4");
+        System.out.println("  decimal: 12");
+        System.out.println("\nshould composed to the argument input.");
+        System.out.println("  86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0000000004563918244f412\n");
     }
 }
