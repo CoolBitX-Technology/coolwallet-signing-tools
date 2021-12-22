410def035096faf604a3399a01e9001fc8106042
lekhanh_sotatek <khanh.le@sotatek.com>
2021-12-23 03:28:10 +0700

    signing/tx parser

--- script-tester/src/App.js
+++ script-tester/src/App.js
@@ -1,24 +1,23 @@
-import React from "react";
-import { HashRouter as Router } from "react-router-dom";
+import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
 import { Container, Row, Col, ButtonGroup, Button } from 'react-bootstrap';
-import WebBleTransport from "@coolwallet/transport-web-ble";
-import * as core from "@coolwallet/core";
-import { getAppKeysOrGenerate } from "./utils/keypairUtil";
-import Routes from "./components/Routes";
+import WebBleTransport from '@coolwallet/transport-web-ble';
+import * as core from '@coolwallet/core';
+import { getAppKeysOrGenerate } from './utils/keypairUtil';
+import Routes from './components/Routes';
 // import MyNavBar from "./components/NavBar";
-import "./App.css";
+import './App.css';
 
 const { appPublicKey, appPrivateKey } = getAppKeysOrGenerate();
-
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { transport: undefined, cardName: "", isLocked: false };
+    this.state = { transport: null, cardName: '', isLocked: false };
   }
 
   componentDidCatch(error, errorInfo) {
-    console.debug("catched error :", error);
+    console.debug('catched error :', error);
   }
 
   setIsLocked = (isLocked) => {
@@ -27,14 +26,14 @@ class App extends React.Component {
 
   connect = async () => {
     WebBleTransport.listen(async (error, device) => {
-      console.log(device)
+      console.log(device);
       if (device) {
         const cardName = device.name;
         const transport = await WebBleTransport.connect(device);
         const SEPublicKey = await core.config.getSEPublicKey(transport);
         this.setState({ transport, cardName, SEPublicKey });
-        localStorage.setItem('cardName', cardName)
-        localStorage.setItem('SEPublicKey', SEPublicKey)
+        localStorage.setItem('cardName', cardName);
+        localStorage.setItem('SEPublicKey', SEPublicKey);
         console.log(`SEPublicKey: ${SEPublicKey}`);
       } else {
         console.log(error);
@@ -44,30 +43,28 @@ class App extends React.Component {
 
   disconnect = () => {
     WebBleTransport.disconnect(this.state.transport.device.id);
-    this.setState({ transport: undefined, cardName: "" });
+    this.setState({ transport: undefined, cardName: '' });
   };
 
   makeConnectComponent(btnName, onClick) {
     return (
-      <Row className="function-component">
+      <Row className='function-component'>
+        <Col xs={2}>{'Card Name'}</Col>
+        <Col className='show-text-area'>{this.state.cardName}</Col>
         <Col xs={2}>
-          {'Card Name'}
-        </Col>
-        <Col className="show-text-area">
-          {this.state.cardName}
-        </Col>
-        <Col xs={2}>
-          <ButtonGroup className="d-flex">
-            <Button variant='light' onClick={onClick}>{btnName}</Button>
+          <ButtonGroup className='d-flex'>
+            <Button variant='light' onClick={onClick}>
+              {btnName}
+            </Button>
           </ButtonGroup>
         </Col>
       </Row>
-    )
-  };
+    );
+  }
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <Router>
           <Container>
             <Row className='title'>
@@ -75,8 +72,7 @@ class App extends React.Component {
             </Row>
             {this.state.transport
               ? this.makeConnectComponent('Disconnect', this.disconnect)
-              : this.makeConnectComponent('Connect', this.connect)
-            }
+              : this.makeConnectComponent('Connect', this.connect)}
           </Container>
           <Routes
             isLocked={this.state.isLocked}
--- script-tester/src/components/Signing/signing.js
+++ script-tester/src/components/Signing/signing.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
-import { makeSendComponentWithTwoInput, makeSendComponentWithThreeInput } from './componentMaker';
+import {
+  makeSendComponentWithTwoInput,
+  makeSendComponentWithThreeInput,
+} from './componentMaker';
 import { apdu } from '@coolwallet/core';
-import { commands } from "../../config/command";
+import { commands } from '../../config/command';
+import TxParser from './txParser';
 const bip39 = require('bip39');
 
 function Signing({ transport, appId, appPrivateKey, isLocked, setIsLocked }) {
@@ -17,17 +21,17 @@ function Signing({ transport, appId, appPrivateKey, isLocked, setIsLocked }) {
   const [utxoOutputArgument, setUtxoOutputArgument] = useState('');
   const [utxoScript, setUtxoScript] = useState('');
   const [utxoSignature, setUtxoSignature] = useState('');
-  const scriptSingature = "FA0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+  const scriptSingature =
+    'FA0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000';
 
   const signTx = async () => {
-    setIsLocked(true)
+    setIsLocked(true);
     try {
-
-      const fullSctipt = script + scriptSingature
+      const fullScript = script + scriptSingature;
 
       const preActions = [];
       const sendScript = async () => {
-        await apdu.tx.sendScript(transport, fullSctipt);
+        await apdu.tx.sendScript(transport, fullScript);
       };
 
       preActions.push(sendScript);
@@ -47,46 +51,42 @@ function Signing({ transport, appId, appPrivateKey, isLocked, setIsLocked }) {
       await apdu.tx.finishPrepare(transport);
 
       // get tx detail
-      await apdu.tx.getTxDetail(transport)
+      await apdu.tx.getTxDetail(transport);
 
       await apdu.tx.clearTransaction(transport);
       await apdu.mcu.control.powerOff(transport);
-      
-      setSignature(encryptedSignature)
-      
-    } catch (error) {
 
+      setSignature(encryptedSignature);
+    } catch (error) {
       console.error(error);
     } finally {
       setIsLocked(false);
     }
-  }
-
+  };
 
   const signUtxoTx = async () => {
-    setIsLocked(true)
+    setIsLocked(true);
     try {
-      console.log("utxoIntputArgument: ", utxoIntputArgument)
-      console.log("utxoOutputArgument: ", utxoOutputArgument)
+      console.log('utxoIntputArgument: ', utxoIntputArgument);
+      console.log('utxoOutputArgument: ', utxoOutputArgument);
 
-      const fullSctipt = utxoScript + scriptSingature
+      const fullScript = utxoScript + scriptSingature;
 
-      console.log("fullSctipt: ", fullSctipt)
+      console.log('fullScript: ', fullScript);
 
-      await apdu.tx.sendScript(transport, fullSctipt);
+      await apdu.tx.sendScript(transport, fullScript);
 
-      await executeScript(
-        transport,
-        utxoOutputArgument
-      )
+      await executeScript(transport, utxoOutputArgument);
 
       // const actions = utxoIntputArgument.map(
       //   (utxoArgument) => async () => {
       //     return executeUtxoScript(transport, appId, appPrivateKey, await utxoArgument);
       //   });
 
-
-      const encryptedSignatureArray = await executeUtxoScript(transport, utxoIntputArgument);
+      const encryptedSignatureArray = await executeUtxoScript(
+        transport,
+        utxoIntputArgument
+      );
 
       // const encryptedSignatureArray = [];
       // eslint-disable-next-line no-await-in-loop
@@ -95,44 +95,44 @@ function Signing({ transport, appId, appPrivateKey, isLocked, setIsLocked }) {
       // }
 
       // encryptedSignatureArray.push(await actions());
-      
-      console.log("encryptedSignatureArray: ", encryptedSignatureArray)
 
-      
+      console.log('encryptedSignatureArray: ', encryptedSignatureArray);
+
       // finish prepare
       await apdu.tx.finishPrepare(transport);
 
       // get tx detail
-      await apdu.tx.getTxDetail(transport)
+      await apdu.tx.getTxDetail(transport);
 
       await apdu.tx.clearTransaction(transport);
       await apdu.mcu.control.powerOff(transport);
 
-      setUtxoSignature(encryptedSignatureArray)
+      setUtxoSignature(encryptedSignatureArray);
     } catch (error) {
-
       console.error(error);
     } finally {
       setIsLocked(false);
     }
-  }
-
+  };
 
   const executeScript = async (transport, argument) => {
-    const { outputData: encryptedSignature, statusCode, msg } = await apdu.execute.executeCommand(
+    const {
+      outputData: encryptedSignature,
+      statusCode,
+      msg,
+    } = await apdu.execute.executeCommand(
       transport,
       commands.EXECUTE_SCRIPT,
       'SE',
       argument,
       undefined,
-      undefined,
+      undefined
     );
-    console.log("encryptedSignature: ", encryptedSignature)
-    console.log("statusCode: ", statusCode)
-    console.log("msg: ", msg)
+    console.log('encryptedSignature: ', encryptedSignature);
+    console.log('statusCode: ', statusCode);
+    console.log('msg: ', msg);
     return encryptedSignature;
   };
-
 
   /**
    * Scriptable step 3
@@ -141,18 +141,22 @@ function Signing({ transport, appId, appPrivateKey, isLocked, setIsLocked }) {
    * @param {*} redeemScriptType (redeemScriptType === ScriptType.P2PKH) ? "10" : "11"
    */
   const executeUtxoScript = async (transport, utxoArgument) => {
-    const { outputData: encryptedSignature, statusCode, msg } = await apdu.execute.executeCommand(
+    const {
+      outputData: encryptedSignature,
+      statusCode,
+      msg,
+    } = await apdu.execute.executeCommand(
       transport,
       commands.EXECUTE_UTXO_SCRIPT,
       'SE',
       utxoArgument,
-      "11",
+      '11',
       undefined
     );
 
-    console.log("encryptedSignature: ", encryptedSignature)
-    console.log("statusCode: ", statusCode)
-    console.log("msg: ", msg)
+    console.log('encryptedSignature: ', encryptedSignature);
+    console.log('statusCode: ', statusCode);
+    console.log('msg: ', msg);
     return encryptedSignature;
   };
 
@@ -163,8 +167,35 @@ function Signing({ transport, appId, appPrivateKey, isLocked, setIsLocked }) {
       <Row className='title2'>
         <p>Scriptable Signing</p>
       </Row>
-      {makeSendComponentWithTwoInput('Signature', Signature, signTx, disabled, argument, setArgument, 'argument', script, setScript, 'script')}
-      {makeSendComponentWithThreeInput('UTXO Signature', utxoSignature, signUtxoTx, disabled, utxoIntputArgument, setUtxoIntputArgument, 'intput arg', utxoOutputArgument, setUtxoOutputArgument, 'output arg', utxoScript, setUtxoScript, 'script')}
+      {makeSendComponentWithTwoInput(
+        'Signature',
+        Signature,
+        signTx,
+        disabled,
+        argument,
+        setArgument,
+        'argument',
+        script,
+        setScript,
+        'script'
+      )}
+      {makeSendComponentWithThreeInput(
+        'UTXO Signature',
+        utxoSignature,
+        signUtxoTx,
+        disabled,
+        utxoIntputArgument,
+        setUtxoIntputArgument,
+        'intput arg',
+        utxoOutputArgument,
+        setUtxoOutputArgument,
+        'output arg',
+        utxoScript,
+        setUtxoScript,
+        'script'
+      )}
+      <TxParser title='General Sign' data={Signature} />
+      <TxParser title='UTXO Sign' data={utxoSignature} />
     </Container>
   );
 }
