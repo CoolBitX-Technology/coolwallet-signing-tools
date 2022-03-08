55c71286d4dfc694ca49ca0cce4390ce1e1dfb59
hao.phan <hao.phan@sotatek.com>
2022-03-08 16:49:52 +0700

    Update script SmartContract

--- script-generator/src/main/java/com/coolbitx/wallet/signing/scriptlib/SolScript.java
+++ script-generator/src/main/java/com/coolbitx/wallet/signing/scriptlib/SolScript.java
@@ -8,8 +8,6 @@ import com.coolbitx.wallet.signing.utils.ScriptAssembler.SignType;
 import com.coolbitx.wallet.signing.utils.ScriptData.Buffer;
 
 public class SolScript {
-    
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -17,8 +15,11 @@ public class SolScript {
  */
     public String script = "03000202C70700000001F5CAA01700CAA11700CAAC170002CAAC170003CAACC7000460CAACC7006410CAAC170074CAAC170075CAACC7007602CAAC170078DDFC970023DAACC7C0790C07D207CC05065052455353425554546F4E";
 
+    public String scriptSmartContract = "03000202C70700000001F5CAA0C70005CAACC7000505CAACC7000A05CAACC7000F02CAACC7001160CAAC570071CAAC170091CAAC170092CAAC170093CAACC7009402CAAC170096CAACC7009704DC07C003534F4CD207C005534D415254D207CC05065052455353425554546F4E";
+
     public static void listAll() {
         System.out.println("Sol: \n" + getSolScript() + "\n");
+        System.out.println("Sol Smart Contract: \n" + getSolSmartScript() + "\n");
     }
 
     public static String getSolScript() {
@@ -55,6 +56,45 @@ public class SolScript {
                 .showMessage("SOL")
                 .showAddress(ScriptData.getDataBufferAll(Buffer.CACHE2, 35))
                 .showAmount(data, 7)
+                .showPressButton()
+                .setHeader(HashType.SHA256, SignType.EDDSA)
+                .getScript();
+        return script;
+    }
+
+    public static String getSolSmartScript() {
+        ScriptArgumentComposer sac = new ScriptArgumentComposer();
+        ScriptData numberRequireSignature = sac.getArgument(5);
+        ScriptData numberReadonlySignedAccount = sac.getArgument(5);
+        ScriptData numberReadonlyUnSignedAccount = sac.getArgument(5);
+        ScriptData keyCount = sac.getArgument(2);
+        ScriptData keys = sac.getArgument(96);
+        ScriptData recentBlockHash = sac.getArgument(32);
+        ScriptData prefixHeader = sac.getArgument(1);
+        ScriptData programIdIndex = sac.getArgument(1);
+        ScriptData keyIndicesCount = sac.getArgument(1);
+        ScriptData keyIndices = sac.getArgument(2);
+        ScriptData dataLength = sac.getArgument(1);
+        ScriptData data = sac.getArgument(4);
+
+        ScriptAssembler scriptAsb = new ScriptAssembler();
+        String script = scriptAsb
+                .setCoinType(0x01f5)
+                .copyArgument(numberRequireSignature)
+                .copyArgument(numberReadonlySignedAccount)
+                .copyArgument(numberReadonlyUnSignedAccount)
+                .copyArgument(keyCount)
+                .copyArgument(keys)
+                .copyArgument(recentBlockHash)
+                .copyArgument(prefixHeader)
+                .copyArgument(programIdIndex)
+                .copyArgument(keyIndicesCount)
+                .copyArgument(keyIndices)
+                .copyArgument(dataLength)
+                .copyArgument(data)
+
+                .showMessage("SOL")
+                .showWrap("SMART", "")
                 .showPressButton()
                 .setHeader(HashType.SHA256, SignType.EDDSA)
                 .getScript();
