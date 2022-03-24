5f73d5c54a91dbc4a0a5db6a973d1d84a2ed697c
lekhanh_sotatek <khanh.le@sotatek.com>
2022-03-24 10:04:00 +0700

    feat(coin-sol): add spl-token, add show address, fix show amount

--- script-generator/src/main/java/com/coolbitx/wallet/signing/scriptlib/SolScript.java
+++ script-generator/src/main/java/com/coolbitx/wallet/signing/scriptlib/SolScript.java
@@ -22,79 +22,76 @@ public class SolScript {
     public String scriptSmartContract = "03000202C70700000001F5CAA0C70005CAACC7000505CAACC7000A05CAACC7000F02CAACC7001160CAAC570071CAAC170091CAAC170092CAAC170093CAACC7009402CAAC170096CAACC7009704DC07C003534F4CD207C005534D415254D207CC05065052455353425554546F4E";
 
     public static void listAll() {
-        System.out.println("Sol: \n" + getSolScript() + "\n");
+        System.out.println("Sol transfer: \n" + getTransferScript() + "\n");
         System.out.println("Sol Smart Contract: \n" + getSolSmartScript() + "\n");
+        System.out.println("Sol transfer spl token: \n" + getTransferSplTokenScript() + "\n");
+
     }
 
-    public static String getSolScript() {
+    public static String getTransferScript() {
         ScriptArgumentComposer sac = new ScriptArgumentComposer();
-        ScriptData numberRequireSignature = sac.getArgument(1);
-        ScriptData numberReadonlySignedAccount = sac.getArgument(1);
-        ScriptData numberReadonlyUnSignedAccount = sac.getArgumentRightJustified(1);
-        ScriptData keyCount = sac.getArgumentRightJustified(1);
-        ScriptData keys = sac.getArgumentRightJustified(96);
-        ScriptData recentBlockHash = sac.getArgumentRightJustified(32);
-        ScriptData prefixHeader = sac.getArgument(1);
-        ScriptData programIdIndex = sac.getArgument(1);
-        ScriptData keyIndicesCount = sac.getArgument(1);
-        ScriptData keyIndices = sac.getArgument(2);
-        ScriptData dataLength = sac.getArgument(2);
+        ScriptData fromAccount = sac.getArgument(32);
+        ScriptData toAccount = sac.getArgument(32);
+        ScriptData programId = sac.getArgument(32);
+        ScriptData recentBlockHash = sac.getArgument(32);
+        ScriptData dataLength = sac.getArgument(1);
+        ScriptData programIdIndex = sac.getArgument(4);
         ScriptData data = sac.getArgumentAll();
 
         ScriptAssembler scriptAsb = new ScriptAssembler();
-
         String script = scriptAsb
                 .setCoinType(0x01f5)
-                .copyArgument(numberRequireSignature)
-                .copyArgument(numberReadonlySignedAccount)
-                .copyArgument(numberReadonlyUnSignedAccount)
-                .copyArgument(keyCount)
-                .copyArgument(keys)
+                .copyString("01")
+                .copyString("00")
+                .copyString("01")
+                .copyString("03")
+                .copyArgument(fromAccount)
+                .copyArgument(toAccount)
+                .copyArgument(programId)
                 .copyArgument(recentBlockHash)
-                .copyArgument(prefixHeader)
-                .copyArgument(programIdIndex)
-                .copyArgument(keyIndicesCount)
-                .copyArgument(keyIndices)
+                .copyString("01")
+                .copyString("02")
+                .copyString("02")
+                .copyString("0001")
                 .copyArgument(dataLength)
+                .copyArgument(programIdIndex)
                 .copyArgument(data)
 
                 .showMessage("SOL")
-                .showAddress(ScriptData.getDataBufferAll(Buffer.CACHE2, 8))
-                .showAmount(data, 12)
+                .baseConvert(toAccount, Buffer.CACHE2, 0, ScriptAssembler.base58Charset,
+                        ScriptAssembler.zeroInherit)
+                .showAddress(ScriptData.getDataBufferAll(Buffer.CACHE2))
+                .clearBuffer(Buffer.CACHE2)
+                .baseConvert(data, Buffer.CACHE1, 8, ScriptAssembler.binaryCharset, ScriptAssembler.inLittleEndian)
+                .showAmount(ScriptData.getDataBufferAll(Buffer.CACHE1), 9)
+                .clearBuffer(Buffer.CACHE1)
                 .showPressButton()
                 .setHeader(HashType.NONE, SignType.EDDSA)
                 .getScript();
+
         return script;
     }
 
     public static String getSolSmartScript() {
         ScriptArgumentComposer sac = new ScriptArgumentComposer();
-        ScriptData numberRequireSignature = sac.getArgument(1);
-        ScriptData numberReadonlySignedAccount = sac.getArgument(1);
-        ScriptData numberReadonlyUnSignedAccount = sac.getArgumentRightJustified(1);
-        ScriptData keyCount = sac.getArgumentRightJustified(1);
         ScriptData keys = sac.getArgumentRightJustified(96);
         ScriptData recentBlockHash = sac.getArgumentRightJustified(32);
-        ScriptData prefixHeader = sac.getArgument(1);
-        ScriptData programIdIndex = sac.getArgument(1);
-        ScriptData keyIndicesCount = sac.getArgument(1);
-        ScriptData keyIndices = sac.getArgument(1);
         ScriptData dataLength = sac.getArgument(1);
         ScriptData data = sac.getArgumentAll();
 
         ScriptAssembler scriptAsb = new ScriptAssembler();
         String script = scriptAsb
                 .setCoinType(0x01f5)
-                .copyArgument(numberRequireSignature)
-                .copyArgument(numberReadonlySignedAccount)
-                .copyArgument(numberReadonlyUnSignedAccount)
-                .copyArgument(keyCount)
+                .copyString("01")
+                .copyString("00")
+                .copyString("01")
+                .copyString("03")
                 .copyArgument(keys)
                 .copyArgument(recentBlockHash)
-                .copyArgument(prefixHeader)
-                .copyArgument(programIdIndex)
-                .copyArgument(keyIndicesCount)
-                .copyArgument(keyIndices)
+                .copyString("01")
+                .copyString("02")
+                .copyString("01")
+                .copyString("01")
                 .copyArgument(dataLength)
                 .copyArgument(data)
 
@@ -103,6 +100,53 @@ public class SolScript {
                 .showPressButton()
                 .setHeader(HashType.NONE, SignType.EDDSA)
                 .getScript();
+        return script;
+    }
+
+    public static String getTransferSplTokenScript() {
+        ScriptArgumentComposer sac = new ScriptArgumentComposer();
+        ScriptData ownerAccount = sac.getArgument(32);
+        ScriptData fromAssociateAccount = sac.getArgument(32);
+        ScriptData toAssociateAccount = sac.getArgument(32);
+        ScriptData programId = sac.getArgument(32);
+
+        ScriptData recentBlockHash = sac.getArgumentRightJustified(32);
+        ScriptData dataLength = sac.getArgument(1);
+        ScriptData programIdIndex = sac.getArgument(1);
+        ScriptData data = sac.getArgumentAll();
+
+        ScriptAssembler scriptAsb = new ScriptAssembler();
+        String script = scriptAsb
+                .setCoinType(0x01f5)
+                .copyString("01")
+                .copyString("00")
+                .copyString("01")
+                .copyString("04")
+                .copyArgument(ownerAccount)
+                .copyArgument(fromAssociateAccount)
+                .copyArgument(toAssociateAccount)
+                .copyArgument(programId)
+                .copyArgument(recentBlockHash)
+                .copyString("01")
+                .copyString("03")
+                .copyString("03")
+                .copyString("020100")
+                .copyArgument(dataLength)
+                .copyArgument(programIdIndex)
+                .copyArgument(data)
+
+                .showWrap("SOL", "SPL")
+                .baseConvert(toAssociateAccount, Buffer.CACHE2, 0, ScriptAssembler.base58Charset,
+                        ScriptAssembler.zeroInherit)
+                .showAddress(ScriptData.getDataBufferAll(Buffer.CACHE2))
+                .clearBuffer(Buffer.CACHE2)
+                .baseConvert(data, Buffer.CACHE1, 8, ScriptAssembler.binaryCharset, ScriptAssembler.inLittleEndian)
+                .showAmount(ScriptData.getDataBufferAll(Buffer.CACHE1), 9)
+                .clearBuffer(Buffer.CACHE1)
+                .showPressButton()
+                .setHeader(HashType.NONE, SignType.EDDSA)
+                .getScript();
+
         return script;
     }
 }
