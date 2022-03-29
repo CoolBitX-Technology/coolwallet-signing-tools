b5fc3a67f140d8aa6ccfa3b462b325e7b18cb061
Quincy Chang <quincy0710@hotmail.com>
2022-03-29 11:12:59 +0800

    feat(ada): CW-6988 support byron address

--- script-generator/src/main/java/com/coolbitx/wallet/signing/main/AllScript.java
+++ script-generator/src/main/java/com/coolbitx/wallet/signing/main/AllScript.java
@@ -6,24 +6,24 @@ public class AllScript {
 
     public static void main(String[] args) throws Exception {
         System.out.println("----- script list -----");
-        AtomScript.listAll();
-        BnbScript.listAll();
-        BtcFamilyScript.listAll();
-        DotScript.listAll();
-        EvmScript.listAll();
-        EthScript.listAll();
-        IcxScript.listAll();
-        TrxScript.listAll();
-        XlmScript.listAll();
-        XrpScript.listAll();
+        // AtomScript.listAll();
+        // BnbScript.listAll();
+        // BtcFamilyScript.listAll();
+        // DotScript.listAll();
+        // EvmScript.listAll();
+        // EthScript.listAll();
+        // IcxScript.listAll();
+        // TrxScript.listAll();
+        // XlmScript.listAll();
+        // XrpScript.listAll();
         AdaScript.listAll();
-        EtcScript.listAll();
-        CronosScript.listAll();
-        CroScript.listAll();
-        XtzScript.listAll();
-        LunaScript.listAll();
-        MaticScript.listAll();
-        TerraScript.listAll();
+        // EtcScript.listAll();
+        // CronosScript.listAll();
+        // CroScript.listAll();
+        // XtzScript.listAll();
+        // LunaScript.listAll();
+        // MaticScript.listAll();
+        // TerraScript.listAll();
     }
 
 }
