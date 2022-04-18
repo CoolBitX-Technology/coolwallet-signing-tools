f52cb179fe0854b2eb1513fd938a2d92420e2cec
AKuzmichkin <akuzmichkin@scnsoft.com>
2022-04-18 15:21:58 +0400

    Global changes

--- script-generator/src/main/java/com/coolbitx/wallet/signing/scriptlib/NearScript.java
+++ script-generator/src/main/java/com/coolbitx/wallet/signing/scriptlib/NearScript.java
@@ -10,6 +10,8 @@ import com.coolbitx.wallet.signing.utils.ScriptAssembler;
 import com.coolbitx.wallet.signing.utils.ScriptData;
 import com.coolbitx.wallet.signing.utils.ScriptAssembler.HashType;
 import com.coolbitx.wallet.signing.utils.ScriptAssembler.SignType;
+import com.coolbitx.wallet.signing.utils.ScriptData.Buffer;
+import com.google.common.base.Strings;
 
 public class NearScript {
 
@@ -20,14 +22,34 @@ public class NearScript {
     public static String getNearScript() {
 
         ScriptArgumentComposer sac = new ScriptArgumentComposer();
-        ScriptData argAmount = sac.getArgumentRightJustified(10);
-        ScriptData argPayload = sac.getArgumentAll();
+        ScriptData signerLength = sac.getArgument(1);
+        ScriptData signer = sac.getArgumentVariableLength(68);
+        ScriptData publicKey = sac.getArgument(32);
+        ScriptData nonce = sac.getArgument(8);
+        ScriptData receiverLength = sac.getArgument(1);
+        ScriptData receiver = sac.getArgumentVariableLength(68);
+        ScriptData blockHash = sac.getArgument(32);
+        ScriptData deposit = sac.getArgument(16);
+        ScriptData amount = sac.getArgument(10);
 
         String script = new ScriptAssembler()
             .setCoinType(0x018d)
-            .copyArgument(argPayload)
+            .copyArgument(signerLength)
+            .copyString("000000")
+            .setBufferInt(signerLength, 2, 64)
+            .copyArgument(signer)
+            .copyString("00")
+            .copyArgument(publicKey)
+            .baseConvert(nonce, Buffer.TRANSACTION, 8, ScriptAssembler.binaryCharset, ScriptAssembler.inLittleEndian)
+            .copyArgument(receiverLength)
+            .copyString("000000")
+            .setBufferInt(receiverLength, 2, 64)
+            .copyArgument(receiver)
+            .copyArgument(blockHash)
+            .copyString("0100000003")
+            .baseConvert(deposit, Buffer.TRANSACTION, 16, ScriptAssembler.binaryCharset, ScriptAssembler.inLittleEndian)
             .showMessage("NEAR")
-            .showAmount(argAmount, 18)
+            .showAmount(amount, 10)
             .showPressButton()
             .setHeader(HashType.SHA256, SignType.EDDSA)
             .getScript();
@@ -38,15 +60,38 @@ public class NearScript {
     public static String getNearStakeScript() {
 
         ScriptArgumentComposer sac = new ScriptArgumentComposer();
-        ScriptData argAmount = sac.getArgumentRightJustified(10);
-        ScriptData argPayload = sac.getArgumentAll();
+        ScriptData signerLength = sac.getArgument(1);
+        ScriptData signer = sac.getArgumentVariableLength(68);
+        ScriptData publicKey = sac.getArgument(32);
+        ScriptData nonce = sac.getArgument(8);
+        ScriptData receiverLength = sac.getArgument(1);
+        ScriptData receiver = sac.getArgumentVariableLength(68);
+        ScriptData blockHash = sac.getArgument(32);
+        ScriptData deposit = sac.getArgument(16);
+        ScriptData validatorPublicKey = sac.getArgument(32);
+        ScriptData amount = sac.getArgument(10);
 
         String script = new ScriptAssembler()
             .setCoinType(0x018d)
-            .copyArgument(argPayload)
+            .copyArgument(signerLength)
+            .copyString("000000")
+            .setBufferInt(signerLength, 2, 64)
+            .copyArgument(signer)
+            .copyString("00")
+            .copyArgument(publicKey)
+            .baseConvert(nonce, Buffer.TRANSACTION, 8, ScriptAssembler.binaryCharset, ScriptAssembler.inLittleEndian)
+            .copyArgument(receiverLength)
+            .copyString("000000")
+            .setBufferInt(receiverLength, 2, 64)
+            .copyArgument(receiver)
+            .copyArgument(blockHash)
+            .copyString("0100000004")
+            .baseConvert(deposit, Buffer.TRANSACTION, 16, ScriptAssembler.binaryCharset, ScriptAssembler.inLittleEndian)
+            .copyString("00")
+            .copyArgument(validatorPublicKey)
             .showMessage("NEAR")
-            .showMessage("STAKE")
-            .showAmount(argAmount, 18)
+            .showMessage("SMART")
+            .showAmount(amount, 10)
             .showPressButton()
             .setHeader(HashType.SHA256, SignType.EDDSA)
             .getScript();
@@ -57,32 +102,49 @@ public class NearScript {
     public static String getNearSCScript() {
         
         ScriptArgumentComposer sac = new ScriptArgumentComposer();
-        ScriptData argAmount = sac.getArgumentRightJustified(10);
-        ScriptData argPayload = sac.getArgumentAll();
+        ScriptData signerLength = sac.getArgument(1);
+        ScriptData signer = sac.getArgumentVariableLength(68);
+        ScriptData publicKey = sac.getArgument(32);
+        ScriptData nonce = sac.getArgument(8);
+        ScriptData receiverLength = sac.getArgument(1);
+        ScriptData receiver = sac.getArgumentVariableLength(68);
+        ScriptData blockHash = sac.getArgument(32);
+        ScriptData methodLength = sac.getArgument(1);
+        ScriptData method = sac.getArgumentVariableLength(68);
+        ScriptData methodArgsLength = sac.getArgument(1);
+        ScriptData methodArgs = sac.getArgumentVariableLength(68);
+        ScriptData gas = sac.getArgument(8);
+        ScriptData deposit = sac.getArgument(16);
+        ScriptData amount = sac.getArgument(10);
 
         String script = new ScriptAssembler()
             .setCoinType(0x018d)
-            .copyArgument(argPayload)
+            .copyArgument(signerLength)
+            .copyString("000000")
+            .setBufferInt(signerLength, 2, 64)
+            .copyArgument(signer)
+            .copyString("00")
+            .copyArgument(publicKey)
+            .baseConvert(nonce, Buffer.TRANSACTION, 8, ScriptAssembler.binaryCharset, ScriptAssembler.inLittleEndian)
+            .copyArgument(receiverLength)
+            .copyString("000000")
+            .setBufferInt(receiverLength, 2, 64)
+            .copyArgument(receiver)
+            .copyArgument(blockHash)
+            .copyString("0100000002")
+            .copyArgument(methodLength)
+            .copyString("000000")
+            .setBufferInt(methodLength, 2, 64)
+            .copyArgument(method)
+            .copyArgument(methodArgsLength)
+            .copyString("000000")
+            .setBufferInt(methodArgsLength, 2, 64)
+            .copyArgument(methodArgs)
+            .baseConvert(gas, Buffer.TRANSACTION, 8, ScriptAssembler.binaryCharset, ScriptAssembler.inLittleEndian)
+            .baseConvert(deposit, Buffer.TRANSACTION, 16, ScriptAssembler.binaryCharset, ScriptAssembler.inLittleEndian)
             .showMessage("NEAR")
-            .showMessage("SMART")
-            .showAmount(argAmount, 18)
-            .showPressButton()
-            .setHeader(HashType.SHA256, SignType.EDDSA)
-            .getScript();
-
-        return script;
-    }
-
-    public static String getNearSCNoAmountScript() {
-        
-        ScriptArgumentComposer sac = new ScriptArgumentComposer();
-        ScriptData argPayload = sac.getArgumentAll();
-
-        String script = new ScriptAssembler()
-            .setCoinType(0x018d)
-            .copyArgument(argPayload)
-            .showMessage("NEAR")
-            .showMessage("SMART")
+            .showMessage("STAKE")
+            .ifEqual(amount, Strings.padStart("", 20, '0'), "", new ScriptAssembler().showAmount(amount, 10).getScript())
             .showPressButton()
             .setHeader(HashType.SHA256, SignType.EDDSA)
             .getScript();
