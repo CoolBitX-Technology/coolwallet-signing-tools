417e7790839a14b1634ced0891c0a50a8e98e764
Tan Ping Yee <pingyee.tan@pactmail.com>
2022-05-05 02:11:18 +0800

    Merge branch 'main' into feature/Mina_Protocol

--- script-generator/src/main/java/com/coolbitx/wallet/signing/scriptlib/MinaScript.java
+++ script-generator/src/main/java/com/coolbitx/wallet/signing/scriptlib/MinaScript.java
@@ -1,126 +1,110 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.coolbitx.wallet.signing.scriptlib;
 
-import com.coolbitx.wallet.signing.utils.HexUtil;
 import com.coolbitx.wallet.signing.utils.ScriptArgumentComposer;
 import com.coolbitx.wallet.signing.utils.ScriptAssembler;
+import com.coolbitx.wallet.signing.utils.ScriptData;
 import com.coolbitx.wallet.signing.utils.ScriptAssembler.HashType;
 import com.coolbitx.wallet.signing.utils.ScriptAssembler.SignType;
-import com.coolbitx.wallet.signing.utils.ScriptData;
 import com.coolbitx.wallet.signing.utils.ScriptData.Buffer;
 
 public class MinaScript {
 
+
+    public enum MinaTxType {
+        SEND, DELEGATE
+    }
+
     public static void listAll() {
-        //method getDelegationScript
-        //method getPaymentScript
-        System.out.println("Mina Payment: \n" + getMinaPaymentScript(false) + "\n");
-        System.out.println("Mina Delegate: \n" + getMinaDelegateScript(false) + "\n");
+        // https://github.com/jspada/mina-signer/blob/main/src/lib.rs
+        int mainNet = 0;
+        int testNet = 1;
+
+        System.out.println("MINA Send : \n" + getMinaScript(MinaTxType.SEND, testNet) + "\n");
+        System.out.println("MINA Delegate: \n" + getMinaScript(MinaTxType.DELEGATE, testNet) + "\n");
     }
+    
+
+    public static String getMinaScript(MinaTxType type, int networkId) {
+        //https://github.com/jspada/mina-signer/blob/main/tests/tests.rs
+        //https://github.com/LeastAuthority/Mina-Signer-SDK/blob/master/example/lib/test/tests.dart
 
 
-    public static String getMinaPaymentScript(boolean isTestnet) {
+        //https://github.com/MinaProtocol/mina/blob/develop/rfcs/0038-rosetta-construction-api.md
+        // Taken from Client-SDK code
+        // type stakeDelegation = {
+        //     to_: publicKey,
+        //     from: publicKey,
+        //     fee: uint64,
+        //     nonce: uint32,
+        //     memo: option(string),
+        //     validUntil: option(uint32),
+        //   };
+        
+        //   type payment = {
+        //     to_: publicKey,
+        //     from: publicKey,
+        //     fee: uint64,
+        //     amount: uint64,
+        //     nonce: uint32,
+        //     memo: option(string),
+        //     validUntil: option(uint32),
+        //   };
+
+        // https://github.com/MinaProtocol/c-reference-signer/blob/master/unit_tests.c
+        int[] tag = {0,0,0};
+
+        if(type == MinaTxType.DELEGATE){
+            tag[2] = 1;
+        }
+
         ScriptArgumentComposer sac = new ScriptArgumentComposer();
-        ScriptData argFrom = sac.getArgumentRightJustified(55);
-        ScriptData argTo = sac.getArgumentRightJustified(55);
-        ScriptData argAmount = sac.getArgument(8);
-        ScriptData argAccountNumber = sac.getArgument(8);
 
-        ScriptData argSequence = sac.getArgument(8);
-        ScriptData argSource = sac.getArgument(8);
-        ScriptData argMemo = sac.getArgument(32);
-        ScriptData argNonce = sac.getArgument(4);
-        ScriptData argFee = sac.getArgument(8);
-        ScriptData argValidUntil = sac.getArgument(4);
-
-
-        ScriptAssembler scriptAsb = new ScriptAssembler();
-        String script = scriptAsb
-                .setCoinType(0x312A)
-                .copyString(HexUtil.toHexString("{\"account_number\":\""))
-                .baseConvert(argAccountNumber, Buffer.TRANSACTION, 0, ScriptAssembler.decimalCharset, ScriptAssembler.leftJustify)
-                .getScript();
-
-        script = (!isTestnet ? scriptAsb.copyString(HexUtil.toHexString("\",\"chain_id\":\"Binance-Chain-Tigris\",\"data\":null,\"memo\":\""))
-                : scriptAsb.copyString(HexUtil.toHexString("\",\"chain_id\":\"Binance-Chain-Nile\",\"data\":null,\"memo\":\"")))
-                .copyRegularString(argMemo)
-                .copyString(HexUtil.toHexString("\",\"msgs\":[{\"inputs\":[{\"address\":\""))
-                .copyRegularString(argFrom)
-                .copyString(HexUtil.toHexString("\",\"coins\":[{\"amount\":"))
-                .baseConvert(argAmount, Buffer.TRANSACTION, 0, ScriptAssembler.decimalCharset, ScriptAssembler.leftJustify)
-                .copyString(HexUtil.toHexString(",\"denom\":\"MINA\"}]}],\"outputs\":[{\"address\":\""))
-                .copyRegularString(argTo)
-                .copyString(HexUtil.toHexString("\",\"coins\":[{\"amount\":"))
-                .baseConvert(argAmount, Buffer.TRANSACTION, 0, ScriptAssembler.decimalCharset, ScriptAssembler.leftJustify)
-                .copyString(HexUtil.toHexString(",\"denom\":\"MINA\"}]}]}],\"sequence\":\""))
-                .baseConvert(argSequence, Buffer.TRANSACTION, 0, ScriptAssembler.decimalCharset, ScriptAssembler.leftJustify)
-                .copyString(HexUtil.toHexString("\",\"source\":\""))
-                .baseConvert(argSource, Buffer.TRANSACTION, 0, ScriptAssembler.decimalCharset, ScriptAssembler.leftJustify)
-                .copyString(HexUtil.toHexString("\"}"))
-                .getScript();
-        script = (!isTestnet ? scriptAsb.showMessage("MINA")
-                : scriptAsb.showWrap("MINA", "TESTNET"))
-                .showAddress(argTo)
-                .showAmount(argAmount, 8)
-                .showPressButton()
-                //version=00 ScriptAssembler.hash=02=sha256 sign=01=ECDSA
-                .setHeader(HashType.SHA256, SignType.ECDSA)//NEED TO BE REPLACE TO SCHNORR and POSEIDON hash
-                .getScript();
-        return script;
-    }
-
-
-    public static String getMinaDelegateScript(boolean isTestnet) {
-        ScriptArgumentComposer sac = new ScriptArgumentComposer();
-        ScriptData argFrom = sac.getArgumentRightJustified(64);
-        ScriptData argTo = sac.getArgumentRightJustified(64);
-        ScriptData argValue = sac.getArgument(8);
-        ScriptData argAccountNumber = sac.getArgument(8);
-
-        ScriptData argSequence = sac.getArgument(8);
-        ScriptData argSource = sac.getArgument(8);
+        ScriptData argTo = sac.getArgument(55);
+        ScriptData argFrom = sac.getArgument(55);
+        ScriptData argAmount = sac.getArgument(10);
+        ScriptData argFee = sac.getArgumentRightJustified(10);
+        ScriptData argNonce = sac.getArgumentRightJustified(8);
         ScriptData argMemo = sac.getArgumentAll();
+        ScriptData argValidUntil = sac.getArgumentRightJustified(10);
 
 
-        ScriptAssembler scriptAsb = new ScriptAssembler();
-        String script = scriptAsb
-                .setCoinType(0x02CA)
-                // set coinType to 02CA
-                .copyString(HexUtil.toHexString("{\"account_number\":\""))
-                .baseConvert(argAccountNumber, Buffer.TRANSACTION, 0, ScriptAssembler.decimalCharset, ScriptAssembler.leftJustify)
+        ScriptAssembler scriptMina = new ScriptAssembler();
+
+        String script = scriptMina
+                .setCoinType(0x312a)
+                //to
+                .copyArgument(argTo)
+                //from
+                .copyArgument(argFrom)
+                //fee
+                .copyArgument(argFee)
+                //nonce
+                .copyArgument(argNonce)
+                //memo
+                .copyArgument(argMemo)
+                //valid until
+                .copyArgument(argValidUntil)
+                .showMessage("Mina Send")
+                .showAddress(ScriptData.getDataBufferAll(Buffer.CACHE1))
                 .getScript();
-        script = (!isTestnet ? scriptAsb.copyString(HexUtil.toHexString("\",\"chain_id\":\"Binance-Chain-Tigris\",\"data\":null,\"memo\":\""))
-                : scriptAsb.copyString(HexUtil.toHexString("\",\"chain_id\":\"Binance-Chain-Nile\",\"data\":null,\"memo\":\"")))
-                .copyRegularString(argMemo)
-                .copyString(HexUtil.toHexString("\",\"msgs\":[{\"inputs\":[{\"address\":\""))
-                .copyRegularString(argFrom)
-                .copyString(HexUtil.toHexString("\",\"coins\":[{\"amount\":"))
-                .baseConvert(argValue, Buffer.TRANSACTION, 0, ScriptAssembler.decimalCharset, ScriptAssembler.leftJustify)
-                .copyString(HexUtil.toHexString(",\"denom\":\"MINA\"}]}],\"outputs\":[{\"address\":\""))
-                .copyRegularString(argTo)
-                .copyString(HexUtil.toHexString("\",\"coins\":[{\"amount\":"))
-                .baseConvert(argValue, Buffer.TRANSACTION, 0, ScriptAssembler.decimalCharset, ScriptAssembler.leftJustify)
-                .copyString(HexUtil.toHexString(",\"denom\":\"MINA\"}]}]}],\"sequence\":\""))
-                .baseConvert(argSequence, Buffer.TRANSACTION, 0, ScriptAssembler.decimalCharset, ScriptAssembler.leftJustify)
-                .copyString(HexUtil.toHexString("\",\"source\":\""))
-                .baseConvert(argSource, Buffer.TRANSACTION, 0, ScriptAssembler.decimalCharset, ScriptAssembler.leftJustify)
-                .copyString(HexUtil.toHexString("\"}"))
+
+                if (type == MinaTxType.DELEGATE) {
+                    script = scriptMina.showMessage("Mina Delegate").getScript();
+                }else if(type == MinaTxType.SEND) {
+                    script = scriptMina
+                            //amount
+                            .copyArgument(argAmount)
+                            .showAmount(argAmount, 9).getScript();
+                }
+
+                script = scriptMina.showPressButton()
+                .setHeader(HashType.Poseidon, SignType.Schnorr)
                 .getScript();
-        script = (!isTestnet ? scriptAsb.showMessage("MINA")
-                : scriptAsb.showWrap("MINA", "TESTNET"))
-                .showAddress(argTo)
-                .showAmount(argValue, 8)
-                .showPressButton()
-                //version=00 ScriptAssembler.hash=02=sha256 sign=01=ECDSA
-                .setHeader(HashType.SHA256, SignType.ECDSA)//NEED TO BE REPLACE TO SCHNORR and POSEIDON hash
-                .getScript();
+
         return script;
     }
 
-    public static String getMinaPaymentScriptSignature = "";
-    public static String getMinaDelegateScriptSignature = "";
+    public static String MinaSendScriptSignature = "";
+    public static String MinaDelegateScriptSignature = "";
+
 }
