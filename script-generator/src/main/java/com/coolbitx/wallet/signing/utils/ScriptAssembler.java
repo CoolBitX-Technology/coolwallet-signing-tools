bb7ae882d8b14172debd581d3018b42bbc229112
Hank Liu <kccc418@gmail.com>
2022-05-24 12:47:45 +0800

    update get hash function parameter

--- script-generator/src/main/java/com/coolbitx/wallet/signing/utils/ScriptAssembler.java
+++ script-generator/src/main/java/com/coolbitx/wallet/signing/utils/ScriptAssembler.java
@@ -15,7 +15,7 @@ import com.coolbitx.wallet.signing.utils.ScriptData.Buffer;
  * @author Hank Liu (hankliu@coolbitx.com)
  */
 public class ScriptAssembler {
-    
+
     public static final String binaryCharset = "binaryCharset";
     public static final String hexadecimalCharset = "hexadecimalCharset";
     public static final String bcdCharset = "bcdCharset";
@@ -24,44 +24,30 @@ public class ScriptAssembler {
     public static final String base32BitcoinCashCharset = "base32BitcoinCashCharset";
     public static final String base58Charset = "base58Charset";
     public static final String extendedCharset = "extendedCharset";
-    
+
     public static final int leftJustify = 0x01;
     public static final int littleEndian = 0x02;
     public static final int zeroInherit = 0x04;
     public static final int bitLeftJustify8to5 = 0x08;
     public static final int inLittleEndian = 0x10;
-    
-//    public static final int SHA1 = 0x01;
-//    public static final int SHA256 = 0x02;
-//    public static final int SHA512 = 0x03;
-//    public static final int SHA3256 = 0x04;
-//    public static final int SHA3512 = 0x05;
-//    public static final int Keccak256 = 0x06;
-//    public static final int Keccak512 = 0x07;
-//    public static final int RipeMD160 = 0x08;
-//    public static final int SHA256RipeMD160 = 0x09;
-//    public static final int DoubleSHA256 = 0x0D;
-//    public static final int CRC16 = 0x0A;
-//    public static final int Blake2b256 = 0x0E;
-//    public static final int Blake2b512 = 0x0F;
-    
+
     private static String firstParameter, secondParameter;
     public static final String throwSEError = "FF00";
-    
+
     private static int argumentOffset = 0;
-    
+
     private versionType version;
     private String script;
-    
+
     public ScriptAssembler() {
         this.version = versionType.version00;
         this.script = "";
     }
-    
+
     public String getScript() {
         return script;
     }
-    
+
     public enum HashType {
         NONE("00"),
         SHA1("01"),
@@ -79,35 +65,35 @@ public class ScriptAssembler {
         Blake2b512("0F"),
         SHA512256("10");
         private final String hashLabel;
-        
+
         private HashType(String hashLabel) {
             this.hashLabel = hashLabel;
         }
-        
+
         public String toString() {
             return hashLabel;
         }
-        
+
         public int toInt() {
             return Integer.parseInt(hashLabel, 16);
         }
     }
-    
+
     public static enum SignType {
         ECDSA("01"),
         EDDSA("02"),
         BIP32EDDSA("03");
         private final String signLabel;
-        
+
         private SignType(String signLabel) {
             this.signLabel = signLabel;
         }
-        
+
         public String toString() {
             return signLabel;
         }
     }
-    
+
     public static enum versionType {
         version00(0, "00"),
         version02(2, "02"),
@@ -116,26 +102,26 @@ public class ScriptAssembler {
         version05(5, "05");
         private final int versionNum;
         private final String versionLabel;
-        
+
         private versionType(int versionNum, String versionLabel) {
             this.versionNum = versionNum;
             this.versionLabel = versionLabel;
         }
-        
+
         public int getVersionNum() {
             return versionNum;
         }
-        
+
         public String toString() {
             return versionLabel;
         }
     }
-    
+
     public ScriptAssembler setHeader(HashType hash, SignType sign) {
         script = "03" + version.versionLabel + hash + sign + script;
         return this;
     }
-    
+
     private static String compose(String command, ScriptData dataBuf, Buffer destBuf, int arg0, int arg1) {
         clearParameter();
         if (dataBuf == null) {
@@ -177,16 +163,16 @@ public class ScriptAssembler {
                 // Throw some exceptions here.
             }
         }
-        
+
         addIntParameter(arg0);
         addIntParameter(arg1);
         return command + firstParameter + secondParameter;
     }
-    
+
     private static void clearParameter() {
         firstParameter = secondParameter = "";
     }
-    
+
     private static void addIntParameter(int i) {
         switch (i) {
             case 0:
@@ -298,7 +284,7 @@ public class ScriptAssembler {
     public ScriptAssembler switchString(ScriptData conditionData, Buffer destinationBuf, String stringArray) {
         String[] strList = stringArray.split(",");
         script += compose("C1", conditionData, destinationBuf, strList.length, 0);
-        
+
         for (int i = 0; i < strList.length; i++) {
             if (strList[i].equals("[]")) {
                 script += "00";
@@ -487,7 +473,7 @@ public class ScriptAssembler {
         if (outputLimit == 0) {
             outputLimit = 64;
         }
-        
+
         String charsetIndex = "0";
         if (charset.equals(binaryCharset)) {
             charsetIndex = "F";
@@ -522,7 +508,8 @@ public class ScriptAssembler {
      * @return
      */
     public ScriptAssembler hash(ScriptData data, Buffer destinationBuf, HashType hashType) {
-        script += compose("5A", data, destinationBuf, hashType.toInt(), 0);
+        int hashIndex = hashType.toInt();
+        script += compose("5A", data, destinationBuf, hashIndex & 0xf, hashIndex >>> 4);
         return this;
     }
 
@@ -829,7 +816,7 @@ public class ScriptAssembler {
     public ScriptAssembler arrayEnd() {
         return arrayEnd(TYPE_PROTOBUF);
     }
-    
+
     public static final int TYPE_PROTOBUF = 0;
     public static final int TYPE_RLP = 1;
     public static final int TYPE_MESSAGE_PACK_MAP = 2;
@@ -877,7 +864,7 @@ public class ScriptAssembler {
         script += compose("A3", data, destinationBuf, 0, 0);
         return this;
     }
-    
+
     public static final byte typeInt = 0;
     public static final byte typeString = 1;
     public static final byte typeBoolean = 2;
@@ -897,7 +884,7 @@ public class ScriptAssembler {
         script += compose("C5", data, destinationBuf, type, 0);
         return this;
     }
-    
+
     public ScriptAssembler insertString(String data) {
         script += data;
         return this;
