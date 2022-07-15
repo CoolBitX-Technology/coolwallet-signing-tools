b4f006ef58495802369455099f5deda65679ad19
hank121314 <hank121314@gmail.com>
2022-07-15 11:12:46 +0800

    feat(Cosmos): CW-6997 Complete Cosmos compatible scripts (#133)

--- script-generator/src/main/java/com/coolbitx/wallet/signing/main/AllScript.java
+++ script-generator/src/main/java/com/coolbitx/wallet/signing/main/AllScript.java
@@ -19,6 +19,7 @@ public class AllScript {
         XrpScript.listAll();
         AdaScript.listAll();
         EtcScript.listAll();
+        CosmosScript.listAll();
         CronosScript.listAll();
         CroScript.listAll();
         XtzScript.listAll();
