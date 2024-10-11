5d7128e1b3d5906f63cbef13eab62b902cc67f86
Brian Yeh <brian.yeh@coolbitx.com>
2024-10-11 15:51:35 +0800

    feat: use production signature

--- script-generator/src/main/java/com/coolbitx/wallet/signing/scriptlib/CustomEvmScript.java
+++ script-generator/src/main/java/com/coolbitx/wallet/signing/scriptlib/CustomEvmScript.java
@@ -93,9 +93,8 @@ public class CustomEvmScript {
         return scriptAssembler.getScript();
     }
 
-    // TODO: use production signature
     public static String TransferScriptSignature = Strings.padStart(
-            "fa0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+            "30450220139187cbe56bd0338fe28b8afeea6880fe0e4a2df26436be074eb527c95f85170221008373a3ee87178a4bce4caedeee955fcfead319d5e951552580f0ef486a0d8671",
             144,
             '0');
 
@@ -163,9 +162,8 @@ public class CustomEvmScript {
         return scriptAssembler.getScript();
     }
 
-    // TODO: use production signature
     public static String ERC20ScriptSignature = Strings.padStart(
-            "fa0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+            "3045022100c36a14ef60bea98590119a621c84fc5962f0e232c5c675a66667617be6403cb2022056cdb47788be550ec25f05474672cdaf115ceffba2a13fb31ab35bd8d7c14c44",
             144,
             '0');
 
@@ -225,9 +223,8 @@ public class CustomEvmScript {
         return scriptAssembler.getScript();
     }
 
-    // TODO: use production signature
     public static String SmartContractScriptSignature = Strings.padStart(
-            "fa0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+            "30450221008bd26f2ae80c316b678433e8e9fa4a1e04d6d87dd1a7be33f7f68867df0c30dd0220708aa67f90928e100ec03c4d350e5c71f96430493d7c86c04171cc105b91811c",
             144,
             '0');
 
@@ -284,9 +281,8 @@ public class CustomEvmScript {
         return scriptAssembler.getScript();
     }
 
-    // TODO: use production signature
     public static String SmartContractSegmentScriptSignature = Strings.padStart(
-            "fa0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+            "304502203ec212a7a74ddecfd09b1992ebb6244a3a7643ea5a9326926de91d9a499b0938022100d37fe100a50cc51537d8ba910d697ba660c75f239ef9190fa7493b28a5d4183e",
             144,
             '0');
 
@@ -316,9 +312,8 @@ public class CustomEvmScript {
         return scriptAssembler.getScript();
     }
 
-    // TODO: use production signature
     public static String TypedDataScriptSignature = Strings.padStart(
-            "fa0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+            "3046022100edb220ef5f538f9a581c91c81502d369322d03d4eeac29e5926438827a1a14dc02210098c9930f917ed6fb72215a3177733d6f9ede7ea55d1117644b0a5cca581f6737",
             144,
             '0');
 
@@ -344,9 +339,8 @@ public class CustomEvmScript {
         return scriptAssembler.getScript();
     }
 
-    // TODO: use production signature
     public static String MessageScriptSignature = Strings.padStart(
-            "fa0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+            "304502200784b5392cceb1fea5f077d90125cd153d871e164c77aefa31ef4b94690f1fd00221009f77e5af2ee01a8c3b484cc0e3cdac77d984ca24664c336142bdf87d70ba7549",
             144,
             '0');
 
@@ -435,9 +429,8 @@ public class CustomEvmScript {
         return scriptAssembler.getScript();
     }
 
-    // TODO: use production signature
     public static String EIP1559TransferScriptSignature = Strings.padStart(
-            "fa0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+            "3044022075004ded6e769a341ddce52c62bcad825c7209486ebe86cbf4cd9748e12f9fb202203e7d925119744262931ad3c56cb6cc35f3588aaccf8d13cb71ac8f229dbdaed5",
             144,
             '0');
 
@@ -511,7 +504,7 @@ public class CustomEvmScript {
     }
 
     public static String EIP1559ERC20ScriptSignature = Strings.padStart(
-            "fa0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+            "3044022034f24075d205fe20041ce69de2df6f4cbb0ba25461a8074475fa72b33e9b6e7f0220553c5439eeda1ecfe4ef416174fb43d3a894557301fcb6b45be20389cac7e2fb",
             144,
             '0');
 
@@ -565,9 +558,8 @@ public class CustomEvmScript {
                 .getScript();
     }
 
-    // TODO: use production signature
     public static String EIP1559SmartContractScriptSignature = Strings.padStart(
-            "fa0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+            "30450221009b806140d5c17c6286e2101c4d841e97d6f1b3226d73a545beefb645b6c16c1702203450279abcb613c5ef6ac49be9217227fd555011a444b61871694951601ff8fc",
             144,
             '0');
 
@@ -620,9 +612,8 @@ public class CustomEvmScript {
         return scriptAssembler.getScript();
     }
 
-    // TODO: use production signature
     public static String EIP1559SmartContractSegmentScriptSignature = Strings.padStart(
-            "fa0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+            "304502201c7513a62629aa7442362004e5bd277a37d539b5dba0517eed10803b5e5ae1d7022100c0978de5e959bfa1fbe13924588c5bb5565b5ec3bc99b7d6648c0414e323c373",
             144,
             '0');
 }
