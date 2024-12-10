a864dfa6b438b78555aad81eba4181884824554b
Brian Yeh <brian.yeh@coolbitx.com>
2024-12-10 15:49:15 +0800

    feat: add SUI script signatures

--- script-generator/src/main/java/com/coolbitx/wallet/signing/scriptlib/SuiScript.java
+++ script-generator/src/main/java/com/coolbitx/wallet/signing/scriptlib/SuiScript.java
@@ -19,8 +19,8 @@ public class SuiScript {
 
     public static void listAll() {
         System.out.println("Sui Smart Contract: \n" + getSmartContractScript(0x310) + "\n");
-        System.out.println("Sui: \n" + getCoinTransferScript(0x310) + "\n");
-        System.out.println("Sui token: \n" + getTokenTransferScript(0x310) + "\n");
+        System.out.println("Sui Coin Transfer: \n" + getCoinTransferScript(0x310) + "\n");
+        System.out.println("Sui Token Transfer: \n" + getTokenTransferScript(0x310) + "\n");
     }
 
     public static String getSmartContractScript(int coinType) {
@@ -42,8 +42,8 @@ public class SuiScript {
         return scriptAssembler.getScript();
     }
 
-    public static String SmartContractScriptSignature = Strings.padEnd(
-            "FA",
+    public static String SmartContractScriptSignature = Strings.padStart(
+            "304502201beb0945e33d5371a304d8a9a504c0bf27c59a6edbd1373291be48b8f8c3aced022100be56eb062e4e8770f6bfd55d7c828084c4bb5bf028eac15190bb95ec12438ad7",
             144,
             '0');
 
@@ -95,8 +95,8 @@ public class SuiScript {
         return scriptAssembler.getScript();
     }
 
-    public static String CoinTransferScriptSignature = Strings.padEnd(
-            "FA",
+    public static String CoinTransferScriptSignature = Strings.padStart(
+            "3044022033641e50266c41c57f3378c12ce225c2836c314a524104a183f54653967ebd7d022057cb7efaaa5d6486943f926bb16c9db0c2ffd5e1a1bbdc3796b8a4891f6cd788",
             144,
             '0');
 
@@ -164,8 +164,8 @@ public class SuiScript {
         return scriptAssembler.getScript();
     }
 
-    public static String TokenTransferScriptSignature = Strings.padEnd(
-            "FA",
+    public static String TokenTransferScriptSignature = Strings.padStart(
+            "3045022100c50dcec26f5e09554621e2a9a02ff3594b109489c5e9fa9de68b901b10fe235b02206ec0227b57574a03e3b4e14a4a86a7ac88fe3099d5c6a500113e3b1cf900adab",
             144,
             '0');
 }
